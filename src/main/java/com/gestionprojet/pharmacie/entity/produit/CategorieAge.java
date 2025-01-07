9f63934ae881045368f4143c14632f5886f0e803
AHSanda2005 <andrianaivosonsanda@gmail.com>
2025-01-07 14:49:49 +0300

    maladie/curration/trancheage

---
+++ src/main/java/com/gestionprojet/pharmacie/entity/produit/CategorieAge.java
@@ -0,0 +1,24 @@
+package com.gestionprojet.pharmacie.entity.produit;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "categorie_age")
+@Data
+public class CategorieAge {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    int agemin;
+    int agemax;
+
+    public CategorieAge() {
+    }
+}
+
